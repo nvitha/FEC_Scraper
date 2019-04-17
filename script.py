31bfb688ff9d4ec7dc9dfe25ffa4739619a3300f
nvitha <nvitha@github.com>
2019-04-16 21:23:58 -0700

    Added some actual code. Still need to use correct types.

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 import pandas
 
-#TODO Actually write some code
+pandas.set_option('display.max_columns',500) # useful for debugging
 
-print(type(pandas))
+with open("data/indiv_header_file.csv") as f:
+    headers = f.read().strip('\n').split(",")
+
+types = {item: object for item in headers}  # passed in as types to
+
+df = pandas.read_csv("data/itcont.txt", delimiter='|', names=headers, dtype=types)  # We will filter
+                                                                                    # out specific types later
+
+# TODO: use proper types and calculate total contributions by a specific person.
